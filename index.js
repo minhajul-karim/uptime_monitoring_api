29268784f26f74d20eb008d46eb6c4e7c3a13308
minhajul-karim <minhajul.kaarim@gmail.com>
2021-02-24 14:54:34 +0600

    Create server

--- index.js
+++ index.js
@@ -4,3 +4,31 @@
  * Author: Minhajul Karim
  * Date: 24/02/2021
  */
+
+// Dependencies
+const http = require('http')
+
+// App object - module scaffolding
+const app = {}
+
+// Configuration
+app.config = {
+  port: 3000,
+}
+
+// Create server
+app.createServer = () => {
+  const server = http.createServer(app.handleReqResponse)
+  server.listen(app.config.port, () => {
+    console.log(`Listening on port ${app.config.port}`)
+  })
+}
+
+// Function that handles request, response
+app.handleReqResponse = (req, res) => {
+  res.write('HI')
+  res.end()
+}
+
+// Start the server
+app.createServer()
